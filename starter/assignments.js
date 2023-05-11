288441e97998efb0de2e052bade9b94b1fb47d94
Mohamedashrath <mohamedashrath@myads.com>
2023-05-11 09:46:59 +0530

    initialcommit

---
+++ starter/assignments.js
@@ -0,0 +1,9 @@
+let countryName = 'india';
+let continentName = 'Asia';
+let populationStrenght = 14000000;
+console.log(countryName);
+console.log(continentName);
+console.log(populationStrenght);
+let isIsland = true;
+let language;
+console.log(typeof isIsland,populationStrenght,countryName,language);
---
+++ starter/script.js
@@ -0,0 +1,59 @@
+/*let js = 'coding';
+console.log(24+50+75-10);
+console.log('Arshath');
+let firstName = 'Arshat';
+console.log(firstName);
+let myFirstJob = 'RecruiterBPO';
+let myCurrentJob = 'Software Developer';
+console.log(myFirstJob);
+console.log(myCurrentJob);
+const now = 2023;
+const ageArshath = now - 1996;
+const ageAashifa = now - 2011;
+console.log(ageArshath,ageAashifa);
+console.log(ageArshath * 2, ageAashifa * 4 , ageArshath * ageAashifa);
+console.log(ageArshath > ageAashifa * 2);
+console.log(typeof ageArshath);
+firstName = 'Mohammed';
+const secondName =  'Arshath';
+console.log(firstName + ' ' + secondName);
+if(ageArshath>ageAashifa ** 2)
+console.log('Im bigger');
+else
+console.log('Im Nothing');*/
+/*const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 95;
+const johnHeight = 1.88;
+
+const bmiMark = markMass/markHeight ** 2;
+const bmiJohn = johnMass/johnHeight ** 2;
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(bmiMark);
+console.log(bmiJohn);
+console.log(markHigherBMI);
+if(bmiMark > bmiJohn)
+console.log(`Mark's BMI (${bmiMark}) is higher than John's(${bmiJohn})!`);
+else
+console.log(`John's BMI${bmiJohn} is higher than Mark's${bmiMark}!`);*/
+/*const firstName = 'Arshath';
+const job = ' HR Recruiter';
+const birthYear = 1996 ;
+const year = 2023;
+const paraGraph = `I'm ${firstName} a ${year - birthYear} years old ${job}`
+console.log(paraGraph);
+console.log(`sorry\n\ for the\n inconvinience`);
+console.log(`Hai
+how are you ?
+Gud Morning`);*/
+const birthYear = '1996';
+console.log(birthYear);
+console.log(Number(birthYear) + 20);
+console.log(String(100));
+console.log(100);
+const age = '18';
+if(age === 18) console.log("It's a good age ");
+if(age == 18) console.log("It's a good age ");
+const goodQueries = prompt("Please write the queries need to improve");
+console.log(goodQueries);
+
